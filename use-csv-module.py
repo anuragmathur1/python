e549baad23e7522a8755ae27897ba3ca0de17845
Anurag Mathur <anuragmathur1@yahoo.com>
2016-11-05 11:58:27 +0000

    changed portfolio.csv and use-csv-module.py

--- use-csv-module.py
+++ use-csv-module.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import csv
 
+total = 0.0
 with open('portfolio.csv','r') as f:
     rows = csv.reader(f)
     headers = next(rows) ## skip the header
     for row in rows:
+        print(row)
         row[2] = int(row[2])
         row[3] = float(row[3])
         total += row[2] * row[3]
